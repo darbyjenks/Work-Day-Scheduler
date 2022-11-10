c7ab36f8bbca199ac578343c67275090b1da6dfe
Darby Johnson <darbyjjohnson1@gmail.com>
2022-11-10 18:50:12 -0500

    added save button emoji

--- assets/script3.js
+++ assets/script3.js
@@ -11,17 +11,17 @@ $.each(hours, function(i, hour){
   // hour, schedule, and save button appending to the row for each hour
   rowEl.append(`
     <div class="col-12 col-md-2 col-sm-2 btn btn-block p-3 my-2 btn-success" style="padding:0px" data-num=${hourText.attr('data-num')}>${hour + "am"}</div> 
-    <input type="text" class="col-12 col-md-8 col-sm-8 btn btn-block p-3 my-2 btn-danger" style="padding:0px" id="reminder">
+    <input type="text" class="col-12 col-md-8 col-sm-8 btn btn-block p-3 my-2 btn-danger" style="padding:0px" id="reminder${hourText.attr('data-num')}">
     </input>
-    <div class="col-12 col-md-2 col-sm-2 btn btn-block p-3 my-2 btn-info" style="padding:0px" id="save">
+    <div class="col-12 col-md-2 col-sm-2 btn btn-block p-3 my-2 btn-info" style="padding:0px" id="save${hourText.attr('data-num')}">💾
     </div> `);
   } else {
     rowEl.append(`
     <div class="col-12 col-md-2 col-sm-2 btn btn-block p-3 my-2 btn-success" style="padding:0px" data-num=${hourText.attr('data-num')}>${hour + "pm"}
     </div>
-    <input type="text" class="col-12 col-md-8 col-sm-8 btn btn-block p-3 my-2 btn-danger" style="padding:0px" id="reminder">
+    <input type="text" class="col-12 col-md-8 col-sm-8 btn btn-block p-3 my-2 btn-danger" style="padding:0px" id="reminder${hourText.attr('data-num')}">
     </input>
-    <div class="col-12 col-md-2 col-sm-2 btn btn-block p-3 my-2 btn-info" style="padding:0px" id="save">
+    <div class="col-12 col-md-2 col-sm-2 btn btn-block p-3 my-2 btn-info" style="padding:0px" id="save${hourText.attr('data-num')}">💾
     </div> `);
   }
 })

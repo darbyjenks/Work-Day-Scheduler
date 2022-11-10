e32a14f74c69b83d2f7dbcb76d5176eac3d00d47
Darby Johnson <darbyjjohnson1@gmail.com>
2022-11-10 18:35:35 -0500

    cleaned up code

--- assets/script3.js
+++ assets/script3.js
@@ -8,19 +8,20 @@ $.each(hours, function(i, hour){
   hourText.attr('data-num', i + 9);
   // debugger;
   if(hourText.attr('data-num') < 12){
-  // hourText.append(`${hour} am`)
-  rowEl.append(`<div class="col-12 col-md-2 col-sm-2 btn btn-block p-3 my-2 btn-success"style="padding:0px" >${hour + "am"}</div> <input type="text" class="col-12 col-md-8 col-sm-8 btn btn-block p-3 my-2 btn-danger"style="padding:0px" id="reminder">
-  </input>
-    <div class="col-12 col-md-2 col-sm-2 btn btn-block p-3 my-2 btn-info"style="padding:0px" id="save">
+  // hour, schedule, and save button appending to the row for each hour
+  rowEl.append(`
+    <div class="col-12 col-md-2 col-sm-2 btn btn-block p-3 my-2 btn-success" style="padding:0px" data-num=${hourText.attr('data-num')}>${hour + "am"}</div> 
+    <input type="text" class="col-12 col-md-8 col-sm-8 btn btn-block p-3 my-2 btn-danger" style="padding:0px" id="reminder">
+    </input>
+    <div class="col-12 col-md-2 col-sm-2 btn btn-block p-3 my-2 btn-info" style="padding:0px" id="save">
     </div> `);
   } else {
-    rowEl.append(`<div class="col-12 col-md-2 col-sm-2 btn btn-block p-3 my-2 btn-success"style="padding:0px" >${hour + "pm"}
-  </div><input type="text" class="col-12 col-md-8 col-sm-8 btn btn-block p-3 my-2 btn-danger"style="padding:0px" id="reminder">
-  </input>
-    <div class="col-12 col-md-2 col-sm-2 btn btn-block p-3 my-2 btn-info"style="padding:0px" id="save">
+    rowEl.append(`
+    <div class="col-12 col-md-2 col-sm-2 btn btn-block p-3 my-2 btn-success" style="padding:0px" data-num=${hourText.attr('data-num')}>${hour + "pm"}
+    </div>
+    <input type="text" class="col-12 col-md-8 col-sm-8 btn btn-block p-3 my-2 btn-danger" style="padding:0px" id="reminder">
+    </input>
+    <div class="col-12 col-md-2 col-sm-2 btn btn-block p-3 my-2 btn-info" style="padding:0px" id="save">
     </div> `);
   }
-  // bodyEl.append(hour);
-  containerEl.append()
-  console.log(hourText.attr('data-num'))
 })
